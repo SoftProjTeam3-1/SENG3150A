a9746b18ffcc7f54a77332f2a45dcb74c8160913
Oscar <oscar.scorgie@gmail.com>
2025-03-26 15:59:20 +1100

    - Fixed register.html page - Integrated the validation functions into the register.html - Added the match password function to validation.js

--- SENG3150-Project/ReactVite- Frontend/src/templates/validation-loader.js
+++ SENG3150-Project/ReactVite- Frontend/src/templates/validation-loader.js
@@ -1,8 +1,30 @@
+/*
+Author: Oscar Scorgie
+Date: 26/03/2025
+Description: This is a file that loads the validations based on the type of form submitted.
+*/
+
+import { validateLogin, validateRegister } from '../lib/validation.js';
 
 
-import { validateForm } from '../validation.js';
+/*
+    handleFormSubmit():
+        Description: When the form is submitted we determine the page it was submitted from
+        and which validation method to use.
 
+        Returns:
+        Returns the validation results whether its true or false and an alert of errors
+*/
 window.handleFormSubmit = function () {
-    return validateForm("emailLogin", "passwordLogin");
+    const currentPage = window.location.pathname;
+
+    //  Determines the page and which validation method to use
+    if (currentPage.includes("login.html")) {
+        console.log("login ");
+        return validateLogin("emailLogin", "passwordLogin");
+    } else if (currentPage.includes("register.html")) {
+        console.log("Register ");
+        return validateRegister("emailLogin", "passwordRegister", "confirmPasswordRegister");
+    }
 };
 
