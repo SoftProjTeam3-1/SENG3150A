d6b3164e0a8c5ba2ea26213e61603e659629c943
AZRA1367 <harrisonarmstrong2005@gmail.com>
2025-04-28 14:00:11 +1000

    Added stored procedures

---
+++ SENG3150-Project/SprintBoot- Backend/demo/src/main/java/com/example/stored_procedures/CreateUser.java
@@ -0,0 +1,63 @@
+package com.example.stored_procedures;
+
+import com.example.entities.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class CreateUser {
+    
+    private String firstName;
+    private String surname;
+    private String email;
+    private boolean verified;
+    private String password;
+
+    public CreateUser(String firstName, String surname, String email, boolean verified, String password){
+        // Constructor logic here
+        this.firstName = firstName;
+        this.surname = surname;
+        this.email = email;
+        this.verified = verified;
+        this.password = password;
+
+        processTransaction();
+    }
+
+    public void processTransaction(){
+        Transaction transaction = null;
+        Session session = null;
+        Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+
+        configuration.addAnnotatedClass(com.example.entities.User.class);
+        StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties()).build();
+        SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+        try{
+            session = sessionFactory.openSession();
+
+            //start a transaction
+            transaction = session.beginTransaction();
+
+            User user = new User(firstName, surname, email, false, password);
+
+            session.persist(user);
+            transaction.commit();
+        } catch (Exception e) {
+            System.out.println("Error has occurred: " + e.toString());
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+
+    }
+}
---
+++ SENG3150-Project/SprintBoot- Backend/demo/src/main/java/com/example/stored_procedures/ValidateUser.java
@@ -0,0 +1,57 @@
+package com.example.stored_procedures;
+
+import com.example.entities.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class ValidateUser {
+
+    private String email;
+    private String password;
+
+    public ValidateUser(String email, String password) {
+        // Constructor logic here
+        this.email = email;
+        this.password = password;
+    }
+
+    public boolean validateUser(){
+        Transaction transaction = null;
+        Session session = null;
+        Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+
+        configuration.addAnnotatedClass(com.example.entities.User.class);
+
+        StandardServiceRegistry serviceRegistry =
+            new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+        SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+        try{
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            User retrievedUser = session.get(User.class, 1);
+
+            if(retrievedUser.getEmail().equals(email) && retrievedUser.getPassword().equals(password)){
+                System.out.println("User is valid");
+                return true;
+            } else {
+                System.out.println("User is invalid");
+                return false;
+            }
+        } catch(Exception e){
+            System.out.println("Error has occurred: " + e.toString());
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+}
