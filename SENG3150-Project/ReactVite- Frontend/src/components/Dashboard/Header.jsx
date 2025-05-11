d0e580b6974397d1ae1acee6ac26f8928f13721a
Oscar <oscar.scorgie@gmail.com>
2025-05-11 16:58:13 +1000

    Dashboard.jsx - Fixed so game session just have notes - Notes save with user inputs
    Header.jsx
    - Now works independently form the dashboard

--- SENG3150-Project/ReactVite- Frontend/src/components/Dashboard/Header.jsx
+++ SENG3150-Project/ReactVite- Frontend/src/components/Dashboard/Header.jsx
@@ -1,10 +1,45 @@
 
 import optionsIMG from "../../assets/options.png";
+import {useEffect, useRef, useState} from "react";
+import {useNavigate} from "react-router-dom";
 
 
 // The header needs references to the const iconRef and handleOptionsClick function to work.
 // iconRef is a reference to the to options
-const Header = ({ handleOptionsClick, iconRef }) => {
+const Header = () => {
+
+    const navigate = useNavigate();
+
+    // Used to show user menus
+    const [showOptions, setShowOptions] = useState(false);
+
+
+    // True or false value determines if options menu is open
+    const handleOptionsClick = () =>{
+        setShowOptions(prev => !prev);
+    }
+
+    // Used to determine if user clicks off menu
+    const iconRef = useRef(null);
+    const optionsRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (
+                showOptions &&
+                optionsRef.current &&
+                !optionsRef.current.contains(event.target) &&
+                iconRef.current &&
+                !iconRef.current.contains(event.target)
+            ) {
+                setShowOptions(false);
+            }
+        };
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [showOptions]);
 
     return (
         <>
@@ -20,7 +55,19 @@ const Header = ({ handleOptionsClick, iconRef }) => {
                     <h1 className="flex items-center mr-4">Select Date</h1>
                 </div>
             </div>
+
+            {showOptions && (
+                <div className="absolute w-1/6 min-h-screen top-20 left-0 bg-gray-600 shadow p-5 text-gray-600 text-2xl text-center flex flex-col items-center z-50" ref={optionsRef}>
+                    <button className="w-full h-20 bg-white rounded-2xl flex flex-col items-center justify-center transition-transform duration-200 ease-in-out hover:scale-105"
+                            onClick={() => navigate('/manage-activities')}>Manage Activities</button><br></br>
+                    <button className="w-full h-20 bg-white rounded-2xl flex flex-col items-center justify-center transition-transform duration-200 ease-in-out hover:scale-105"
+                            onClick={() => navigate('/attendance')}>Attendance</button><br></br>
+                    <button className="w-full h-20 bg-white rounded-2xl flex flex-col items-center justify-center transition-transform duration-200 ease-in-out hover:scale-105">Logout</button>
+                </div>
+            )}
+
         </>
+
     )
 }
 
--- SENG3150-Project/ReactVite- Frontend/src/components/Dashboard/HomeDashboard.jsx
+++ SENG3150-Project/ReactVite- Frontend/src/components/Dashboard/HomeDashboard.jsx
@@ -3,8 +3,10 @@ import { useState, useEffect, useRef } from "react";
 import DatePicker from "react-datepicker";
 import { format } from 'date-fns';
 import "react-datepicker/dist/react-datepicker.css";
-import { useNavigate } from 'react-router-dom';
 import Header from "./Header.jsx";
+
+
+
 
 
 
@@ -13,34 +15,66 @@ const HomeDashboard = () => {
     //Need to add variables for category's and activities
     //Sync to user when they log in
 
-    const navigate = useNavigate();
 
-    const [selectedType, setSelectedType] = useState(null);
-    //const [notes, setNotes] = useState({});
 
+    // Temporary session which holds data before creation to db
+    const [temporarySession, setTemporarySession] = useState({ id:null, date:null, type:null, activities:[], notes:""});
+
+    // Stores sessions from the database
+    const [sessions, setSessions] = useState([]);
+
+    // Stores selected local sessions the user has clicked
+    const [selectedSessions, setSelectedSessions] = useState([]);
+
+    // Used to show user menus
+    const [showActivitieScreen, setShowActivitieScreen] = useState(false);
     const [showCalendar, setShowCalendar] = useState(false);
-    const [newDate, setNewDate] = useState(null);
+    const [showSessionTypeScreen, setSessionTypeScreen] = useState(false);
 
-    const [dateSelected, setDateSelected] = useState([])
-    const [dateClicked, setDateClicked] = useState([])
 
-    const [showOptions, setShowOptions] = useState(false);
-    const [showSessionType, setSessionType] = useState(false);
-
-    const optionsRef = useRef(null);
+    // Used to determine if user clicks off menu
     const sessionRef = useRef(null);
-    const iconRef = useRef(null);
 
-    // Functionality for when the + button is clicked
-    const handleAddDate = (newDate) =>{
-        const newId = crypto.randomUUID();  //Generate an ID for a date
-        setDateSelected(prev => [...prev, {id: newId, value: newDate}])
-    }
+
+    //Functions for setting a temporary session
+    const setTemporarySessionID = () => {
+        setTemporarySession(prev => ({ ...prev, id: crypto.randomUUID() }));
+    };
+
+    const setTemporarySessionType = (type) => {
+        setTemporarySession(prev => ({ ...prev, type }));
+    };
+
+    const setTemporarySessionDate = (date) => {
+        setTemporarySession(prev => ({ ...prev, date }));
+    };
+
+    // Creates a new session on the db
+    const createSession = async (session) => {
+        // const res = await fetch('http://localhost:8080/api/sessions', {
+        //     method: 'POST',
+        //     headers: { 'Content-Type': 'application/json' },
+        //     body: JSON.stringify(session)
+        // });
+        // const newSession = await res.json();
+
+        const newSession = { ...session }; // this creates a shallow copy
+        setSessions(prev => [...prev, newSession]);
+
+        // Reset teh temp session so no previous data is saved
+        setTemporarySession({
+            id: null,
+            date: null,
+            type: null,
+            activities: [],
+            notes: ""
+        });
+    };
+
 
     // Functionality for when a date box is clicked
-    const handleClickAddDate = (id) =>{
-
-        setDateClicked(prev =>
+    const handleClickSelectedSessions = (id) =>{
+        setSelectedSessions(prev =>
             prev.includes(id)
                 ? prev.filter(clickedId => clickedId !== id) // unselect
                 : [...prev, id] // select
@@ -49,42 +83,48 @@ const HomeDashboard = () => {
 
     // Functionality for when date box is right-clicked to be removed
     const handleRemoveDate = (id) =>{
-        setDateSelected(prev => prev.filter(dateObj => dateObj.id !== id));
-        setDateClicked(prev => prev.filter(clickedId => clickedId !== id));
+        setSelectedSessions(prev => prev.filter(dateObj => dateObj.id !== id));
+        setSelectedSessions(prev => prev.filter(clickedId => clickedId !== id));
     }
 
-    const handleOptionsClick = () =>{
-        setShowOptions(prev => !prev);
-    }
-
+    // Sets the session ID
     const handleNewActivity = () =>{
-        setSessionType(prev => !prev);
+        setTemporarySessionID();
+        setSessionTypeScreen(prev => !prev);
     }
 
+    // Sets the Session type and opens calendar
     const handleSessionSelect = (type) => {
-        setSelectedType(type);
-        setSessionType(false);
+        setTemporarySessionType(type);
+        setSessionTypeScreen(false);
         setShowCalendar(true);
     };
 
+    const updateNotesForSession = (id, newNotes) => {
+        setSessions(prev =>
+            prev.map(session =>
+                session.id === id ? { ...session, notes: newNotes } : session
+            )
+        );
+    };
+
+    const handleActivitieScreenClick = () => {
+        setShowActivitieScreen(prev => ! prev);
+    }
+
     useEffect(() => {
+        // fetch('http://localhost:8080/api/sessions')
+        //     .then(res => res.json())
+        //     .then(data => setSessions(data))
+        //     .catch(err => console.error("Error loading sessions", err));
+
         const handleClickOutside = (event) => {
             if (
-                showOptions &&
-                optionsRef.current &&
-                !optionsRef.current.contains(event.target) &&
-                iconRef.current &&
-                !iconRef.current.contains(event.target)
-            ) {
-                setShowOptions(false);
-            }
-
-            if (
-                showSessionType &&
+                showSessionTypeScreen &&
                 sessionRef.current &&
                 !sessionRef.current.contains(event.target)
             ) {
-                setSessionType(false);
+                setSessionTypeScreen(false);
             }
         };
 
@@ -92,25 +132,27 @@ const HomeDashboard = () => {
         return () => {
             document.removeEventListener("mousedown", handleClickOutside);
         };
-    }, [showOptions, showSessionType]);
+    }, [showSessionTypeScreen]);
 
     return(
         <div className="w-full m-0 p-0">
 
-            <Header handleOptionsClick={handleOptionsClick} iconRef={iconRef}/>
+            <Header/>
 
             <div id="middleSegment" style={{display: 'flex'}} className="bg-emerald-100 min-h-screen">
                 <div id="verticalBar" className=" w-25 bg-gray-600  text-2xl flex flex-col items-center ">
                     <ul className="text-center flex flex-col items-center relative top-[10px]">
-                        {dateSelected.map(({id, value}) => {
-                            const [month, day] = value.split(' ');
-                            const isClicked = dateClicked.includes(id);
+                        {sessions.map(({id, date}) => {
+                            if (!date) return null;
+
+                            const [month, day] = date.split(' ');
+                            const isClicked = selectedSessions.includes(id);
                             return (
                             <button
                                 key={id}
                                 className={`h-20 w-20 border-5 rounded-2xl flex flex-col items-center justify-center leading-none transition-transform duration-200 ease-in-out hover:scale-105
                                 ${isClicked ? 'bg-orange-300 border-orange-600' : 'bg-white border-gray-600'}`}
-                                onClick={() => handleClickAddDate(id)}
+                                onClick={() => handleClickSelectedSessions(id)}
                                 onContextMenu={(e) => {
                                     e.preventDefault(); // prevent browser context menu
                                     handleRemoveDate(id);
@@ -136,42 +178,70 @@ const HomeDashboard = () => {
 
                 <div id="userDisplay">
                     <div className="p-5 flex flex-wrap gap-4">
-                        {dateSelected
-                            .filter(dateObj => dateClicked.includes(dateObj.id))
-                            .map(({ id, value }) => {
-                                const [month, day] = value.split(' ');
-
+                        {sessions.filter(dateObj => selectedSessions.includes(dateObj.id))
+                            .map(({ id, date, type, notes }) => {
+                                const [month, day] = date.split(' ');
                                 return (
                                     <div className="flex flex-col gap-y-5">
-                                        <div
-                                            key={id}
-                                            className="w-75 h-140 bg-white rounded-2xl flex flex-col items-center text-black"
-                                        >
-                                            <div className="text-xl w-full text-center py-3">{month} {day}</div>
 
-                                            <div id="activities" className="w-full h-24 flex items-center justify-center">
-                                                <button
-                                                    className=" text-6xl w-70 h-24 bg-emerald-100 shadow-lg rounded-2xl flex items-center justify-center transition-transform duration-200 ease-in-out hover:scale-105"
-                                                    onClick={handleNewActivity}
+                                        {type === 'game' ? (
+                                            <>
+                                            <div
+                                                key={id}
+                                                className="w-75 h-140 bg-white rounded-2xl flex flex-col items-center text-black"
+                                            >
+                                                <div className="text-xl w-full text-center py-3">{month} {day}</div>
+
+                                                <div
+                                                    className="text-l py-3 w-75 h-125 bg-white rounded-2xl flex flex-col items-center text-black"
                                                 >
-                                                    <div className="leading-none -mt-2">
-                                                        +
+                                                    Notes
+                                                    <div>
+                                                        <textarea
+                                                            className="w-65 h-110 resize-none py-3"
+                                                            placeholder="Write something here..."
+                                                            value={notes}
+                                                            onChange={(e) => updateNotesForSession(id, e.target.value)}
+                                                        />
                                                     </div>
-                                                </button>
+                                                </div>
                                             </div>
-                                        </div>
+                                            </>
+                                        ) : (
+                                            <>
+                                                <div
+                                                    key={id}
+                                                    className="w-75 h-140 bg-white rounded-2xl flex flex-col items-center text-black"
+                                                >
+                                                    <div className="text-xl w-full text-center py-3">{month} {day}</div>
 
-                                        <div
-                                            className="text-l py-3 w-75 h-125 bg-white rounded-2xl flex flex-col items-center text-black"
-                                        >
-                                            Notes
-                                            <div>
-                                                <textarea
-                                                    className="w-65 h-110 resize-none py-3"
-                                                    placeholder="Write something here...">
-                                                </textarea>
-                                            </div>
-                                        </div>
+                                                    <div id="activities" className="w-full h-24 flex items-center justify-center">
+                                                        <button
+                                                            className=" text-6xl w-70 h-24 bg-emerald-100 shadow-lg rounded-2xl flex items-center justify-center transition-transform duration-200 ease-in-out hover:scale-105"
+                                                            onClick={() => handleActivitieScreenClick()}
+                                                        >
+                                                            <div className="leading-none -mt-2">
+                                                                +
+                                                            </div>
+                                                        </button>
+                                                    </div>
+                                                </div>
+
+                                                <div
+                                                    className="text-l py-3 w-75 h-125 bg-white rounded-2xl flex flex-col items-center text-black"
+                                                >
+                                                    Notes
+                                                    <div>
+                                                        <textarea
+                                                            className="w-65 h-110 resize-none py-3"
+                                                            placeholder="Write something here..."
+                                                            value={notes}
+                                                            onChange={(e) => updateNotesForSession(id, e.target.value)}
+                                                        />
+                                                    </div>
+                                                </div>
+                                            </>
+                                        )}
                                     </div>
                                 );
                             })}
@@ -179,16 +249,25 @@ const HomeDashboard = () => {
                 </div>
             </div>
 
+            {showActivitieScreen && (
+                <div className="absolute w-1/6 min-h-screen top-20 left-0 bg-red-400 shadow p-5 text-gray-600 text-2xl text-center flex flex-col items-center">
+                    hello
+                </div>
+            )}
+
             {showCalendar && (
                 <div className="fixed inset-0 bg-opacity-30 backdrop-blur-sm flex justify-center items-center z-50">
                     <div className="bg-white p-6 rounded-xl shadow-xl scale-150">
                         <DatePicker
                             inline
-                            selected={newDate}
                             onChange={(date) => {
-                                setNewDate(date);
                                 const formatted = format(date, 'MMM dd');
-                                handleAddDate(formatted);
+                                setTemporarySessionDate(formatted);
+                                const fullSession = {
+                                    ...temporarySession,
+                                    date: formatted};
+                                setTemporarySession(fullSession); // store latest version
+                                createSession(fullSession);
                                 setShowCalendar(false); // close after picking
                             }}
                         />
@@ -196,20 +275,10 @@ const HomeDashboard = () => {
                 </div>
             )}
 
-            {showOptions && (
-                <div className="absolute w-1/6 min-h-screen top-20 left-0 bg-gray-600 shadow p-5 text-gray-600 text-2xl text-center flex flex-col items-center " ref={optionsRef}>
-                    <button className="w-full h-20 bg-white rounded-2xl flex flex-col items-center justify-center transition-transform duration-200 ease-in-out hover:scale-105"
-                            onClick={() => navigate('/manage-activities')}>Manage Activities</button><br></br>
-                    <button className="w-full h-20 bg-white rounded-2xl flex flex-col items-center justify-center transition-transform duration-200 ease-in-out hover:scale-105"
-                    onClick={() => navigate('/attendance')}>Attendance</button><br></br>
-                    <button className="w-full h-20 bg-white rounded-2xl flex flex-col items-center justify-center transition-transform duration-200 ease-in-out hover:scale-105">Logout</button>
-                </div>
-            )}
-
-            {showSessionType &&(
+            {showSessionTypeScreen &&(
                 <div className="absolute w-1/6 min-h-screen top-20 left-0 bg-gray-600 shadow p-5 text-gray-600 text-2xl text-center flex flex-col items-center" ref={sessionRef}>
                     <button onClick={() => handleSessionSelect('training')} className="w-full h-20 bg-white rounded-2xl flex flex-col items-center justify-center transition-transform duration-200 ease-in-out hover:scale-105">Training Session</button><br></br>
-                    <button className="w-full h-20 bg-white rounded-2xl flex flex-col items-center justify-center transition-transform duration-200 ease-in-out hover:scale-105">Game Session</button><br></br>
+                    <button onClick={() => handleSessionSelect('game')} className="w-full h-20 bg-white rounded-2xl flex flex-col items-center justify-center transition-transform duration-200 ease-in-out hover:scale-105">Game Session</button><br></br>
                 </div>
             )}
         </div>
