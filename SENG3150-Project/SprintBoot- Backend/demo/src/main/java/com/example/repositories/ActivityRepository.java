0fc584191e61c986c6e4b995542fba0b8b2a478c
AZRA1367 <harrisonarmstrong2005@gmail.com>
2025-05-24 15:52:11 +1000

    added update queries to jpa repositories

--- SENG3150-Project/SprintBoot- Backend/demo/src/main/java/com/example/repositories/ActivityRepository.java
+++ SENG3150-Project/SprintBoot- Backend/demo/src/main/java/com/example/repositories/ActivityRepository.java
@@ -1,9 +1,15 @@
 package com.example.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 import com.example.entities.Activity;
 import com.example.entities.ActivityType;
+
+import jakarta.transaction.Transactional;
 
 public interface ActivityRepository extends JpaRepository<Activity, Integer>{
     Activity findById(int id);
@@ -14,4 +20,16 @@ public interface ActivityRepository extends JpaRepository<Activity, Integer>{
     List<Activity> findByDuration(String duration);
     List<Activity> findByFavourite(boolean favourite);
     List<Activity> findByActivityType(ActivityType activityType);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Activity a SET a.peopleRequired = :peopleRequired, a.name = :name, a.description = :description, a.favourite = :favourite, a.duration = :duration WHERE a.id = :id")
+    void updateActivity(
+        @Param("id") int id,
+        @Param("peopleRequired") int peopleRequired,
+        @Param("name") String name,
+        @Param("description") String description,
+        @Param("favourite") boolean favourite,
+        @Param("duration") String duration
+    );
 }
--- SENG3150-Project/SprintBoot- Backend/demo/src/main/java/com/example/repositories/ActivityTypeRepository.java
+++ SENG3150-Project/SprintBoot- Backend/demo/src/main/java/com/example/repositories/ActivityTypeRepository.java
@@ -1,12 +1,27 @@
 package com.example.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 import com.example.entities.ActivityType;
+
+import jakarta.transaction.Transactional;
 
 public interface ActivityTypeRepository extends JpaRepository<ActivityType, Integer> {
     ActivityType findById(int id);
     List<ActivityType> findByName(String name);
     ActivityType findDistinctByName(String name);
     List<ActivityType> findByDescription(String description);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE ActivityType a SET a.name = :name, a.description = :description WHERE a.id = :id")
+    void updateActivityType(
+        @Param("id") int id,
+        @Param("name") String name,
+        @Param("description") String description
+    );   
 }
