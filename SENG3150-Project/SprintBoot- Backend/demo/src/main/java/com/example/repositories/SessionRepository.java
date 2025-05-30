e9dcfd1322c6a7cedd2c58219d9fb3b74915fa20
AZRA1367 <harrisonarmstrong2005@gmail.com>
2025-05-30 13:52:15 +1000

    add session activity backend

--- SENG3150-Project/SprintBoot- Backend/demo/src/main/java/com/example/repositories/SessionRepository.java
+++ SENG3150-Project/SprintBoot- Backend/demo/src/main/java/com/example/repositories/SessionRepository.java
@@ -2,6 +2,7 @@ package com.example.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Date;
@@ -18,6 +19,9 @@ public interface SessionRepository extends JpaRepository<Session, Integer>{
     List<Session> findTop3ByTypeOrderByDateDesc(SessionType type);
     Session findTopByTypeOrderByDateDesc(SessionType type);
 
-    @Query("SELECT s.sessionID FROM Session s WHERE s.date = :date")
-    int findIdByDate(String date);
+    @Query(value="SELECT s.sessionID FROM Session s WHERE s.date = :date", nativeQuery = true)
+    int findIdByDate(@Param("date") String date);
+
+    @Query(value = "SELECT s.* FROM session s INNER JOIN session_type st ON st.session_typeid = s.session_typeid WHERE st.name = :typeName AND s.date = :date", nativeQuery = true)
+    Session findSessionByDateAndType(@Param("typeName") String typeName, @Param("date") Date date);
 }
