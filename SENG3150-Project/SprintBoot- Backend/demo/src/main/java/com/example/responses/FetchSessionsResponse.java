b762c865b957df7bf64b9302b2dc18881a6778b2
Oscar <oscar.scorgie@gmail.com>
2025-08-07 14:54:01 +1000

    I missed these files in the previous commit

---
+++ SENG3150-Project/SprintBoot- Backend/demo/src/main/java/com/example/responses/FetchSessionsResponse.java
@@ -0,0 +1,57 @@
+package com.example.responses;
+
+
+import com.example.entities.SessionActivity;
+import com.example.entities.SessionType;
+
+import java.util.Date;
+import java.util.List;
+
+public class FetchSessionsResponse {
+    private int sessionID;
+    private Date date;
+    private String sessionTypeId;
+    private List<SessionActivity> activities;
+
+    public FetchSessionsResponse() {}
+
+    public FetchSessionsResponse(int sessionID, Date date, SessionType sessionTypeId, List<SessionActivity> activities) {
+        this.sessionID = sessionID;
+        this.date = date;
+        this.sessionTypeId = sessionTypeId.getName();
+        this.activities = activities;
+    }
+
+    public int getSessionID() {
+        return sessionID;
+    }
+
+    public void setSessionID(int sessionID) {
+        this.sessionID = sessionID;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getSessionTypeId() {
+        return sessionTypeId;
+    }
+
+    public void setSessionTypeId(String sessionTypeId) {
+        this.sessionTypeId = sessionTypeId;
+    }
+
+    public List<SessionActivity> getActivities() {
+        return activities;
+    }
+
+    public void setActivities(List<SessionActivity> activities) {
+        this.activities = activities;
+    }
+}
+
--- SENG3150-Project/SprintBoot- Backend/demo/src/main/java/com/example/responses/LoginResponse.java
+++ SENG3150-Project/SprintBoot- Backend/demo/src/main/java/com/example/responses/LoginResponse.java
@@ -1,14 +1,18 @@
 package com.example.responses;
 
+import com.example.entities.User;
+
 public class LoginResponse {
 
     private boolean response;
     private String message;
+    private User user;
 
     public LoginResponse(){}
-    public LoginResponse(boolean response, String message) {
+    public LoginResponse(boolean response, String message, User user) {
         this.response = response;
         this.message = message;
+        this.user = user;
     }
 
     public boolean isResponse() {
@@ -26,4 +30,11 @@ public class LoginResponse {
     public void setMessage(String message) {
         this.message = message;
     }
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+
 }
