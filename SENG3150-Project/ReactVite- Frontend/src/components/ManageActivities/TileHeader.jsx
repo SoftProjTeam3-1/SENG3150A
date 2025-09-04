10f2ddadf8196816b8ce58bb2592f60970c00de1
AZRA1367 <harrisonarmstrong2005@gmail.com>
2025-09-04 16:26:09 +1000

    cypress tests for activity types completed

--- SENG3150-Project/ReactVite- Frontend/src/components/ManageActivities/TileHeader.jsx
+++ SENG3150-Project/ReactVite- Frontend/src/components/ManageActivities/TileHeader.jsx
@@ -35,7 +35,7 @@ export const TileHead = ({ categoryName, onHeaderClick }) => {
     return (
         <>
             <div className="relative group px-2 py-1">
-                <span>{categoryName}</span>
+                <span id="categoryName">{categoryName}</span>
                 <button
                     className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity duration-200"
                     onClick={handleConfirmation}
