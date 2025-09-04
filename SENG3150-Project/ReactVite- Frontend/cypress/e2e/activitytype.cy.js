5a4c4fa82d362b624e62483cd1aab3810864a903
AZRA1367 <harrisonarmstrong2005@gmail.com>
2025-09-04 15:20:02 +1000

    first cypress test written, needs debugging

---
+++ SENG3150-Project/ReactVite- Frontend/cypress/e2e/activitytype.cy.js
@@ -0,0 +1,29 @@
+
+/// <reference types="cypress" />
+
+describe('Activity Type Create Flow',() => {
+    beforeEach(() => {
+        cy.visit('http://localhost:5173/manage-activities'); // adjust if route differs
+    });
+
+    it('creates a new activity type succesfully with backend active', () => {
+        const categoryName = 'E2E Category';
+
+        cy.intercept('POST', '/api/activityType/create').as('createActivtyType');
+
+        cy.get('input[placeholder="Enter New Category Name"]').type(categoryName);
+
+        cy.get('button[type="submit"]').click();
+
+        cy.wait('@createActivtyType').then((interception) => {
+            expect(interception.request.body).to.deep.equal({ 
+                name: categoryName, 
+                description: '' 
+            });
+
+            expect(interception.response.body).to.have.property('message', "ActivityType created successfully");
+
+            expect(interception.response.statusCode).to.equal(200);
+        });
+    }
+}
