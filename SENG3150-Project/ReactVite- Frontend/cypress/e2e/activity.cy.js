3df2ee440e3ad52278b8e3000e8dd8e167d3d7d0
AZRA1367 <harrisonarmstrong2005@gmail.com>
2025-09-05 12:24:42 +1000

    update activity cypress test written

--- SENG3150-Project/ReactVite- Frontend/cypress/e2e/activity.cy.js
+++ SENG3150-Project/ReactVite- Frontend/cypress/e2e/activity.cy.js
@@ -54,5 +54,45 @@ describe("Activity Update Flow", () => {
         cy.visit("http://localhost:5173/manage-activities");
     });
 
-    
+    it("updates an existing activity with the backend active", () => {
+        const activityNameChanged = "E2E Activity- edited";
+        const activityDescriptionChanged = "This is an activity created during E2E testing -edited";
+        const activityDurationChanged = "20";
+        const activityPeopleRequiredChanged = "6";
+        const activityTypeDescriptionChanged = null;
+
+        cy.intercept('POST', '/api/activity/update').as('updateActivity');
+
+        cy.get('span#categoryName').first().invoke('text').then((categoryName) => {
+            //click on activity to edit it
+            cy.get('li').first().click();
+
+            //type in input fields 
+            cy.get('input[placeholder="Enter activity name"]').type(activityNameChanged);
+            cy.get('textarea[placeholder="Enter activity description"]').type(activityDescriptionChanged);
+            cy.get('input[placeholder="Enter the number of people required"]').type(activityPeopleRequiredChanged);
+            cy.get('input[placeholder="Enter time in minutes"]').type(activityDurationChanged);
+            //submit the form
+            cy.contains('button', "Save").click();
+
+            //wait for post request and assert response and requests
+            cy.wait('@updateActivity').then((interception) => {
+                expect(interception.request.body).to.include({
+                    name: activityNameChanged,
+                    description: activityDescriptionChanged,
+                    duration: activityDurationChanged,
+                    peopleRequired: activityPeopleRequiredChanged,
+                });
+
+                expect(interception.request.body.activityType).to.deep.include({
+                    name: categoryName,
+                    description: null
+                });
+
+                expect(interception.response.body).to.have.property('message', "Activity updated successfully");
+                expect(interception.response.statusCode).to.equal(200);
+            });
+        });
+
+    });
 });
