9819abdb8b1bd46e10cecbaf1fd8eff9c401667f
AZRA1367 <harrisonarmstrong2005@gmail.com>
2025-09-12 14:21:11 +1000

    template added to

--- SENG3150-Project/SprintBoot- Backend/demo/src/test/java/com/example/session_controllers/SessionActivityServiceTest.java
+++ SENG3150-Project/SprintBoot- Backend/demo/src/test/java/com/example/session_controllers/SessionActivityServiceTest.java
@@ -4,29 +4,38 @@ import com.example.entities.Session;
 import com.example.entities.Activity;
 import com.example.entities.SessionActivity;
 import com.example.entities.SessionType;
+import com.example.repositories.SessionActivityRepository;
 import com.example.entities.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.sql.Date;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class SessionActivityServiceTest {
+
+    @Mock
+    private SessionActivityRepository sessionActivityRepository;
+
+    private MockMvc mockMvc;
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(sessionActivityRepository).build();
+        objectMapper = new ObjectMapper();
+
+        //create objects 
+        
+        Activity activity1 = new Activity();
+        Session session1 = new Session();
+        SessionActivity sessionActivity1 = new SessionActivity();
+        //session
+        //activity
+    }
     
     /*
      * DELETE SESSION ACTIVITY TESTS FOR SESSIONACTIVITY SERVICE
