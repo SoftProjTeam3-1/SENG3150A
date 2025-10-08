7de8cea8a9ee047f38bf360b40edb0e012fee396
oscar <oscar.scorgie@gmail.com>
2025-10-08 06:56:02 +0000

    Finished 4 files of JavaDoc

--- SENG3150-Project/SprintBoot- Backend/demo/src/main/java/com/example/controllers/ActivityTypeRequest.java
+++ SENG3150-Project/SprintBoot- Backend/demo/src/main/java/com/example/controllers/ActivityTypeRequest.java
@@ -1,12 +1,22 @@
 package com.example.controllers;
 
+/**
+ * The Request template for {@link ActivityTypeRequest}
+ */
 public class ActivityTypeRequest {
     private String activityType;
 
+    /**
+     * Set the activity type
+     * @param activityType The activityType being stored.
+     */
     public void setActivityType(String activityType) {
         this.activityType = activityType;
     }
 
+    /**
+     * Returns the activityType
+     */
     public String getActivityType() {
         return activityType;
     }
